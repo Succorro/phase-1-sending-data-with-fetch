08b5bc3ff5b13d0d69a22930df9a4e163c240838
Steven Gutierrez <stevogbmv@gmail.com>
2022-12-18 06:57:14 -0800

    commit tree 4e4efd820f3a83dfb2ebdcb0830b9db3351f4201

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Add your code here
+// const formData = {
+//     dogName: 'Byron',
+//     dogBreed: 'Poodle',
+// };
+
+// const configurationObject = {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type' : 'application/json',
+//         'Accept': 'application/json'
+//     }, 
+//     body: JSON.stringify(formData),
+// };
+// fetch('http://localhost:3000/dogs',configurationObject)
+// .then(response => response.json())
+// .then(object => console.log(object))
+// .catch(function (error) {
+//     alert("Did bad ting mans!");
+//     console.log(error.message)
+// })
+///////////////////////////////////////////
+const data = {
+    name:'Steve' ,
+    email: 'steve@steve.com' ,
+};
+const configurationData = {
+    method: 'POST',
+    headers: {
+        'Content-Type' : 'application/json',
+        'Accept' : 'application/json'
+    },
+    body: JSON.stringify(data)
+}
+let message = 'Unauthorized Access'
+function submitData (userName, userEmail){
+    fetch('http://localhost:3000/users', configurationData)
+    .then(response => response.json())
+    .then(obj => document.body.innerHTML = obj['id'])
+    .catch((error) => document.body.innerHTML = error.message)
+}
+submitData();
